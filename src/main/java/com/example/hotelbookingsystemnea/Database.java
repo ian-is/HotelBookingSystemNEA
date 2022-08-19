9797d8886128d87b361efe559988fe92c9b972c3
ianischisiu <ianischisiu@gmail.com>
2022-08-19 18:45:33 +0100

    database updated

--- src/main/java/com/example/hotelbookingsystemnea/Database.java
+++ src/main/java/com/example/hotelbookingsystemnea/Database.java
@@ -10,24 +10,19 @@ public class Database {
     public static void SQLTest() {
 
         String DatabaseLocation = System.getProperty("user.dir") + "\\Database1.accdb";
-
-
+        //employee
         try {
             Connection con = DriverManager.getConnection("jdbc:ucanaccess://" + DatabaseLocation, "", "");
-
             Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
-
             String sql = "SELECT * FROM employee";
-
-
             ResultSet rs1 = stmt.executeQuery(sql);
 
             while (rs1.next()) {
-                System.out.println("EmployeeID : " + rs1.getString("First name") + "" + rs1.getString("surname") + "" + rs1.getString("password"));
+                System.out.println("employeeID : " + rs1.getString("First name") + " " + rs1.getString("surname") + " " + rs1.getString("password"));
             }
+
             rs1.close();
             con.close();
-
 
             }catch (Exception e) {
             System.out.println("Error in the SQL class:" + e);
