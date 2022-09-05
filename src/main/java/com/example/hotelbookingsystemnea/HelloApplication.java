61dc75a6d99c49407682e4e026b6699d0a0b997b
ianischisiu <ianischisiu@gmail.com>
2022-09-05 15:18:24 +0100

    updated

--- src/main/java/com/example/hotelbookingsystemnea/HelloApplication.java
+++ src/main/java/com/example/hotelbookingsystemnea/HelloApplication.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("startPage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
@@ -22,6 +22,11 @@ public class HelloApplication extends Application {
     public static void main(String[] args) {
         launch();
         Database.SQLTest();
+
+
+
+
+
 
 
     }
--- src/main/java/com/example/hotelbookingsystemnea/HelloController.java
+++ src/main/java/com/example/hotelbookingsystemnea/HelloController.java
@@ -1,18 +1,61 @@
 // code which operates the screen and makes it interactive
-
-
-
 package com.example.hotelbookingsystemnea;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+    private Stage stage;
+
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void switchToEmployeeLoginPage (ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("EmployeeLogin.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setScene(scene);
+        stage.show();
+
     }
-}
+    @FXML
+    public void returnToStartPage(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("startPage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
